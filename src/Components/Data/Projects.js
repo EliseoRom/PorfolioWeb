4b9246d690c8b4d5e63f925b46ff7fe47af063bc
Eliseo Joaquín Romero <78494234+EliseoRom@users.noreply.github.com>
2021-10-27 00:04:47 -0300

    Update Projects.js

--- src/Components/Data/Projects.js
+++ src/Components/Data/Projects.js
@@ -1,6 +1,15 @@
 export const ProjectsData = [
-    {
+        {
         id: 1,
+        title: 'E-Market',
+        about: 'The app was created with a development team at Henry bootcamp It has all the functionalities of a virtual supermarket and admin panel',
+        tags: ['React', 'NodeJs', 'express', 'HTML', 'JavaScript', 'materialUI', 'MongoDB', 'Redux', 'CSS'],
+        github: 'https://github.com/EliseoRom/Henry-PF',
+        demo: 'https://e-market-front-chappetm.vercel.app/promotions#',
+        image: require('../Utils/Icons/market.jpg').default
+    },
+    {
+        id: 2,
         title: 'Video Games Collection',
         demo: 'https://api-videogames.vercel.app/',
         about: 'Application made in the exam instance of the bootcamp soyHenry, the app uses a Videogames API. Technologies used',
@@ -9,15 +18,6 @@ export const ProjectsData = [
         image: require('../Utils/Icons/games.png').default
         
     
-    },
-    {
-        id: 2,
-        title: 'E-Market',
-        about: 'The app was created with a development team at Henry bootcamp It has all the functionalities of a virtual supermarket and admin panel',
-        tags: ['React', 'NodeJs', 'express', 'HTML', 'JavaScript', 'materialUI', 'MongoDB', 'Redux', 'CSS'],
-        github: 'https://github.com/EliseoRom/Henry-PF',
-        demo: 'https://e-market-front-chappetm.vercel.app/promotions#',
-        image: require('../Utils/Icons/market.jpg').default
     },
     {
         id: 3,
